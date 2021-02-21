5917f03dbd7fad8e4c56f1e9ac755f0df3e494ae
QasimRRizvi <syed.rizvi2385@gmail.com>
2021-02-21 16:31:59 +0500

    Update commands positions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // git init Usage: git init
+// Add remote Usage: git remote add origin git@github.com:User/UserRepo.git.
 // git add. Usage: git add [file] ...
 // git config Usage: git config user.
 // git commit. Usage: git commit -m “[ Type in the commit message]” ...
-// git status Usage: git status
 // git log. Usage: git log. ...
+// git status Usage: git status
+// git push. Usage: git push origin main.
+// git pull. Usage: git pull origin main.
 // git branch. Usage: git branch. ...
 // git checkout. Usage: git checkout [branch name] ...
-// Add remote Usage: git remote add origin git@github.com:User/UserRepo.git.
-// git push. Usage: git push [variable name] master.
-// git pull. Usage: git pull [variable name] master.
 
 // for help visit this link http://guides.beanstalkapp.com/version-control/common-git-commands.html
 
